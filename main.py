aabe56ee907622803e66d8ccd994fd64e84fa8b8
Attmosfears <deschmalo@icloud.com>
2025-03-16 21:26:30 +0100

    Ajout des collisions avec le decor et les bordures de l'ecran

--- main.py
+++ main.py
@@ -10,13 +10,30 @@ class Game:
         self.running = True
         self.clock = pygame.time.Clock()
 
-        # Chargement du joueur
-        self.player = Player(240, 160)
+        # Chargement de la carte
+        tmx_data = pytmx.util_pygame.load_pygame("Assets/Map/Maptest.tmx")
+        map_date = pyscroll.data.TiledMapData(tmx_data)
+        map_layer = pyscroll.orthographic.BufferedRenderer(map_date, self.screen.get_size())
 
-        self.tmx_data = pytmx.util_pygame.load_pygame("Assets/Map/Maptest.tmx")
-        map_data = pyscroll.data.TiledMapData(self.tmx_data)
-        map_layer = pyscroll.orthographic.BufferedRenderer(map_data, self.screen.get_size())
+        #Chargement du joueur
+        self.player = Player(30,30)
+
+        """Gestion des collisions"""
+        # Recuperation des rectangles de collision dans une liste
+        self.walls = []
+        for obj in tmx_data.objects:
+            if obj.type == "collision":
+                self.walls.append(pygame.Rect(obj.x, obj.y, obj.width, obj.height))
+        # Ajout des bords de l'écran comme des collisions
+        screen_width, screen_height = self.screen.get_size()
+        self.walls.append(pygame.Rect(-5, 0, 5, screen_height))
+        self.walls.append(pygame.Rect(screen_width, 0, 5, screen_height))
+        self.walls.append(pygame.Rect(0, -5, screen_width, 5))
+        self.walls.append(pygame.Rect(0, screen_height, screen_width, 5))
+
+        #Dessiner le groupe de calques
         self.group = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer=2)
+        self.group.add(self.player)
 
     def handling_events(self):
         for event in pygame.event.get():
@@ -38,37 +55,25 @@ class Game:
             self.player.velocity[1] = 0
 
     def update(self):
-        # Sauvegarde de la position précédente
-        old_position = self.player.rect.copy()
 
         # Déplacement du joueur
         self.player.move()
+        self.player.update()
 
-        # Récupération des dimensions de l'écran
-        screen_width, screen_height = self.screen.get_size()
-
-        # Supposons que le joueur a une taille (width, height) dans son rect
-        player_width = self.player.rect.width
-        player_height = self.player.rect.height
-
-        # Vérification des bordures
-        if self.player.rect.left < 0:
-            self.player.rect.left = 0
-        if self.player.rect.right > screen_width:
-            self.player.rect.right = screen_width
-        if self.player.rect.top < 0:
-            self.player.rect.top = 0
-        if self.player.rect.bottom > screen_height:
-            self.player.rect.bottom = screen_height
+        #Verification de collision
+        for sprite in self.group.sprites():
+            if sprite.feet.collidelist(self.walls) > -1:
+                sprite.move_back()
 
     def display(self):
-        # Dessiner le groupe de calques
+        self.group.update()
+        self.group.center(self.player.rect.center)
         self.group.draw(self.screen)
-        self.player.draw(self.screen)
         pygame.display.flip()
 
     def run(self):
         while self.running:
+            self.player.save_location()
             self.handling_events()
             self.update()
             self.display()
@@ -76,6 +81,6 @@ class Game:
 
 pygame.init()
 pygame.display.set_caption("Pixel-Alchemist")
-screen = pygame.display.set_mode((960, 640))
+screen = pygame.display.set_mode((320, 224))
 game = Game(screen)
 game.run()
--- player.py
+++ player.py
@@ -1,15 +1,35 @@
 import pygame
 
-class Player:
+class Player(pygame.sprite.Sprite):
     def __init__(self, x, y):
-        self.image = pygame.image.load('Assets/Art/playerTest.png').convert()
-        self.rect = self.image.get_rect(x=x, y=y)
+        super().__init__()
+        self.sprite_sheet = pygame.image.load('Assets/Art/playerTest.png').convert()
+        self.image = self.get_image(0,0)
+        self.rect = self.image.get_rect()
+        self.position = [x, y]
         self.speed = 5
         self.velocity = [0, 0]
+        self.feet = pygame.Rect(0,0, self.rect.width * 0.5, 12)
+        self.old_position = self.position.copy()
+
+
+
+    def save_location(self): self.old_position = self.position.copy()
+
+    def get_image(self, x, y):
+        image = pygame.Surface([16, 16])
+        image.blit(self.sprite_sheet, (0, 0), (x, y, 16, 16))
+        return image
 
     def move(self):
-        self.rect.move_ip(self.velocity[0] * self.speed, self.velocity[1] * self.speed)
-        pass
+        self.position[0] += self.velocity[0] * self.speed
+        self.position[1] += self.velocity[1] * self.speed
 
-    def draw(self, screen):
-        screen.blit(self.image, self.rect)
+    def move_back(self):
+        self.position = self.old_position
+        self.rect.topleft = self.position
+        self.feet.midbottom = self.rect.midbottom
+
+    def update(self):
+        self.rect.topleft = self.position
+        self.feet.midbottom = self.rect.midbottom
