6c5d471786223e6a8dde3b0de32a0f6fc485ad9f
RomainVid <romain.videau@efrei.net>
2025-05-09 19:03:58 +0200

    MAP 2main

---
+++ Assets/Map/Attack_2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Attack_2" tilewidth="32" tileheight="32" tilecount="64" columns="16">
+ <image source="../../../Tile sett/craftpix-net-316309-golem-character-sprite-sheets-pixel-art/Lava_golem/Attack_2.png" width="512" height="128"/>
+</tileset>
