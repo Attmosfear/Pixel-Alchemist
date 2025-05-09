fb6257e4cf4b955315576ad2ff387aaf885473a7
RomainVid <romain.videau@efrei.net>
2025-05-09 17:58:05 +0200

    Tiled

---
+++ Assets/Map/Labo.tsx
@@ -0,0 +1,53 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Labo" tilewidth="32" tileheight="32" tilecount="735" columns="35">
+ <image source="../../../Tile sett/craftpix-net-791281-medieval-interior-top-down-pixel-art-tileset/All Tileset/32x32.png" width="1120" height="672"/>
+ <tile id="220">
+  <animation>
+   <frame tileid="220" duration="800"/>
+   <frame tileid="222" duration="800"/>
+   <frame tileid="224" duration="800"/>
+  </animation>
+ </tile>
+ <tile id="221">
+  <animation>
+   <frame tileid="221" duration="800"/>
+   <frame tileid="223" duration="800"/>
+   <frame tileid="225" duration="800"/>
+  </animation>
+ </tile>
+ <tile id="255">
+  <animation>
+   <frame tileid="255" duration="800"/>
+   <frame tileid="257" duration="800"/>
+   <frame tileid="259" duration="800"/>
+  </animation>
+ </tile>
+ <tile id="256">
+  <animation>
+   <frame tileid="256" duration="800"/>
+   <frame tileid="258" duration="800"/>
+   <frame tileid="260" duration="800"/>
+  </animation>
+ </tile>
+ <tile id="601">
+  <animation>
+   <frame tileid="601" duration="800"/>
+   <frame tileid="602" duration="800"/>
+   <frame tileid="531" duration="1600"/>
+  </animation>
+ </tile>
+ <tile id="641">
+  <animation>
+   <frame tileid="641" duration="800"/>
+   <frame tileid="642" duration="800"/>
+   <frame tileid="643" duration="800"/>
+  </animation>
+ </tile>
+ <tile id="676">
+  <animation>
+   <frame tileid="676" duration="800"/>
+   <frame tileid="677" duration="800"/>
+   <frame tileid="678" duration="800"/>
+  </animation>
+ </tile>
+</tileset>
--- Assets/Map/laboratoire.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="Laboratoire" tilewidth="32" tileheight="32" tilecount="735" columns="35">
- <image source="../../../Tile sett/craftpix-net-791281-medieval-interior-top-down-pixel-art-tileset/All Tileset/32x32.png" width="1120" height="672"/>
-</tileset>
