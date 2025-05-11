9aa015a88b58fb05986ebe57a53844437da8da31
RomainVid <romain.videau@efrei.net>
2025-05-11 17:57:34 +0200

    MAP 2main

---
+++ Assets/Map/Cave.tsx
@@ -0,0 +1,46 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Cave" tilewidth="32" tileheight="32" tilecount="598" columns="26">
+ <image source="Main 1/Cave/Cave.png" width="832" height="736"/>
+ <tile id="140">
+  <animation>
+   <frame tileid="140" duration="800"/>
+   <frame tileid="141" duration="800"/>
+   <frame tileid="142" duration="800"/>
+  </animation>
+ </tile>
+ <tile id="166">
+  <animation>
+   <frame tileid="166" duration="800"/>
+   <frame tileid="167" duration="800"/>
+   <frame tileid="168" duration="800"/>
+  </animation>
+ </tile>
+ <tile id="304">
+  <animation>
+   <frame tileid="304" duration="800"/>
+   <frame tileid="306" duration="800"/>
+   <frame tileid="308" duration="800"/>
+  </animation>
+ </tile>
+ <tile id="305">
+  <animation>
+   <frame tileid="305" duration="800"/>
+   <frame tileid="307" duration="800"/>
+   <frame tileid="309" duration="800"/>
+  </animation>
+ </tile>
+ <tile id="330">
+  <animation>
+   <frame tileid="330" duration="800"/>
+   <frame tileid="332" duration="800"/>
+   <frame tileid="334" duration="800"/>
+  </animation>
+ </tile>
+ <tile id="331">
+  <animation>
+   <frame tileid="331" duration="800"/>
+   <frame tileid="333" duration="800"/>
+   <frame tileid="335" duration="800"/>
+  </animation>
+ </tile>
+</tileset>
